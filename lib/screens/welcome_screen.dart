27bb9802966341639393dd4d054e4421b81c872a
nargok <nargok@gmail.com>
2020-04-11 19:49:29 +0900

    named route

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  String id = 'welcome_screen';
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
