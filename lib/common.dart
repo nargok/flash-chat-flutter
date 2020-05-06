39481c0a0291b06c2b25edb30d7ba1556b44b617
nargok <nargok@gmail.com>
2020-05-06 13:38:07 +0900

    implement AuthorizationHeaderBuilder class

--- lib/common.dart
+++ lib/common.dart
@@ -63,8 +63,7 @@ class AuthorizationHeader {
     }
     params.addAll(_additionalParameters);
     if (!params.containsKey('oauth_signature')) {
-      params['oauth_signature'] =
-          _createSignature(_method, _url, params);
+      params['oauth_signature'] = _createSignature(_method, _url, params);
     }
 
     final String authHeader = 'OAuth ' +
@@ -139,13 +138,63 @@ class AuthorizationHeader {
     final String consumerSecret =
         Uri.encodeComponent(_clientCredentials.tokenSecret);
     final String tokenSecret = _credentials != null
-      ? Uri.encodeComponent(_credentials.tokenSecret)
-      : '';
+        ? Uri.encodeComponent(_credentials.tokenSecret)
+        : '';
     final String signingKey = '$consumerSecret&$tokenSecret';
 
     //
     // Calculating the signature
     //
     return _signatureMethod.sign(signingKey, base.toString());
+  }
+}
+
+class AuthorizationHeaderBuilder {
+  SignatureMethod _signatureMethod;
+  ClientCredentials _clientCredentials;
+  Credentials _credentials;
+  String _method;
+  String _url;
+  Map<String, String> _additionalParameters;
+
+  AuthorizationHeaderBuilder();
+
+  AuthorizationHeaderBuilder.from(AuthorizationHeaderBuilder other)
+      : _signatureMethod = other._signatureMethod,
+        _clientCredentials = other._clientCredentials,
+        _credentials = other._credentials,
+        _method = other._method,
+        _url = other._url,
+        _additionalParameters = other._additionalParameters;
+
+  set signatureMethod(SignatureMethod value) => _signatureMethod = value;
+
+  set clientCredentials(ClientCredentials value) => _clientCredentials = value;
+
+  set credentials(Credentials value) => _credentials = value;
+
+  set method(String value) => _method = value;
+
+  set url(String value) => _url = value;
+
+  set additionalParameters(Map<String, String> value) =>
+      _additionalParameters = value;
+
+  AuthorizationHeader build() {
+    if (_signatureMethod == null) {
+      throw StateError('signatureMethod is not set');
+    }
+    if (_clientCredentials == null) {
+      throw StateError('clientCredentials is not set');
+    }
+    if (_method == null) {
+      throw StateError('method is not set');
+    }
+    if (_url == null) {
+      throw StateError('url is not set');
+    }
+
+    return AuthorizationHeader(_signatureMethod, _clientCredentials,
+        _credentials, _method, _url, _additionalParameters);
   }
 }
